42d6c600b0c646b142a3592e595d1654529cf78d
Lena Schettler <lena@schettler.net>
2024-02-19 18:11:12 +0100

    Music player buttons

--- frog.py
+++ frog.py
@@ -5,25 +5,49 @@ from glob import glob
 WIDTH=400
 HEIGHT=400
 
+play_button = Actor('pause', (50, 350))
+play_button.scale = 0.1
+next_button = Actor('next', (110, 350))
+next_button.scale = 0.1
+
 tunes = [m[6:] for m in glob('music/*')]
 tune = 0
 print (tunes)
 
-def on_key_down(key, mod, unicode):
+def next_tune():
     global tune
+
+    tune += 1
+    tune = tune % len(tunes)
+    print("TUNE=", tune)
+    music.play(tunes[tune])  
+
+
+def on_key_down(key, mod, unicode):
 
     #print(key, mod, unicode)
     if key == keys.SPACE:
         if music.is_playing(True):
             music.pause()
+            play_button.image = 'play'
+            play_button.scale = 0.1
+
         else:
             music.unpause()
-    elif key == keys.RIGHT:
-        tune += 1
-        tune = tune % len(tunes)
-        print("TUNE=", tune)
-        music.play(tunes[tune])  
+            play_button.image = 'pause'
+            play_button.scale = 0.1
 
+    elif key == keys.RIGHT:
+        next_tune()
+
+def on_mouse_down(pos):
+    print(pos)
+
+    #(82, 321)
+    #(139, 376)
+
+    if 82 <= pos[0] <= 321 and 139 <= pos[1] <= 376:
+        next_tune()         
 
 
 music.play(tunes[tune])  
@@ -36,6 +60,8 @@ frog.images = images
 
 def draw():
     frog.draw()
+    play_button.draw()
+    next_button.draw()
 
 counter = 0
 
